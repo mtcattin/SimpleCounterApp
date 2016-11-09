a102f775b6011daa61945f29ef6520309ee31611
mtcattin <mtcattin@gmail.com>
2016-11-09 09:25:44 -0700

    Initial commit

---
+++ app/src/main/java/com/example/matthew/simplecounterapp/MainSettings.java
@@ -0,0 +1,13 @@
+package com.example.matthew.simplecounterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_settings);
+    }
+}
