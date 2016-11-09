647e275cbe6d570ec7498f64d81b7bc1db1dbaa4
mtcattin <mtcattin@gmail.com>
2016-11-09 13:44:57 -0700

    stuff

--- app/src/main/java/com/example/matthew/simplecounterapp/MainSettings.java
+++ app/src/main/java/com/example/matthew/simplecounterapp/MainSettings.java
@@ -9,6 +9,5 @@ public class MainSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_settings);
-
     }
 }
