1781be7a54df7beea23982a0a0a47db78f315f7e
mtcattin <mtcattin@gmail.com>
2016-11-09 09:52:30 -0700

    stuff

---
+++ app/src/main/java/com/example/matthew/simplecounterapp/CounterDataBase.java
@@ -0,0 +1,12 @@
+package com.example.matthew.simplecounterapp;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Matthew on 11/9/2016.
+ */
+
+public class CounterDataBase extends SQLiteOpenHelper {
+    private static final String TABLE_NAME = "counter";
+
+}
